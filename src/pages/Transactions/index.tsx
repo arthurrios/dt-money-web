7227146e8000720830357a1423621511e6e53197
arthur <arthur.rios007@gmail.com>
2024-02-24 12:32:09 -0300

    feat: add table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Web development</td>
+              <td>
+                <PriceHighlight variant="income">$ 7.000,00</PriceHighlight>
+              </td>
+              <td>Sale</td>
+              <td>02/24/2024</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- $ 12,40</PriceHighlight>
+              </td>
+              <td>Food</td>
+              <td>02/17/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
